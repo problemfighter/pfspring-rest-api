b7f353f638fee20afd59f613af2dd1c89430e0e0
Touhid Mia <hmtm.cse@gmail.com>
2020-09-14 14:54:35 +0600

    Refactoring and Documentation

---
+++ src/main/java/com/problemfighter/pfspring/restapi/query/SearchOperation.java
@@ -0,0 +1,12 @@
+package com.problemfighter.pfspring.restapi.query;
+
+public enum SearchOperation {
+    GREATER_THAN,
+    LESS_THAN,
+    GREATER_THAN_EQUAL,
+    LESS_THAN_EQUAL,
+    NOT_EQUAL,
+    EQUAL,
+    MATCH,
+    MATCH_END,
+}
